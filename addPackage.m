5b5afdd297688022750e2e55026cc02976f3cd07
Mark-C-Anderson <anderson.mark.az@gmail.com>
2020-10-19 16:51:52 -0600

    initial commit

---
+++ addPackage.m
@@ -0,0 +1,51 @@
+% addPackage(pathToPackage,packageName)
+% 
+% This is a function that adds a MATLAB package to your path. There are
+% three cases:
+%
+% 1. Add a package that already exists in the userpath folder
+%       - pathToPackage = just the name of the package (eg.
+%       'General-Signal-Processing')
+%       - packageName = empty
+%
+% 2. Add a package that exists in a specified path on your computer
+%       - pathToPackage = the absolute path to the package on your computer
+%       - packageName = empty
+%
+% 3. Clone a package from an online Git repository
+%       - pathToPackage = the cloning url
+%       - packageName = the name you want to give the package. If left
+%       empty, it defaults to the default cloning name.
+%
+% Author: Mark C. Anderson
+% Institution: Brigham Young University
+
+function addPackage(pathToPackage,packageName)
+    
+    % 1. Seeing if the package exists in the user directory (Documents/MATLAB)
+    if isfolder(strcat(userpath,filesep,pathToPackage))
+        addpath(genpath(strcat(userpath,filesep,pathToPackage)));
+        disp(strcat('Added: ',strcat(userpath,filesep,pathToPackage)))
+    
+    % 2. Seeing if the package exists elsewhere on the computer
+    elseif isfolder(pathToPackage)
+        addpath(genpath(pathToPackage))
+        disp(strcat('Added: ',pathToPackage))
+    
+    % 3. Clone the package from an online Git repository 
+    else
+        disp(strcat("Cloning ",pathToPackage," into ",userpath))
+        
+        if nargin < 2
+            separators = strfind(pathToPackage,filesep); % Find all of the file separators
+            finalSlash = separators(end); % Choose the final file separator
+            packageName = pathToPackage(finalSlash + 1:end - 4); % Take the text after the final
+                                                                 % file separator and before
+                                                                 % the '.git'
+        end
+        
+        eval(strcat("!git clone ",convertCharsToStrings(pathToPackage)," ",userpath,filesep,convertCharsToStrings(packageName)))
+        
+    end
+
+end
