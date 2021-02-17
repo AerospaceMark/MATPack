56996b145c1e00a1863ead85594b979dbd35db99
Mark C. Anderson <anderson.mark.az@gmail.com>
2021-02-17 14:29:29 -0700

    initial commit

---
+++ src/runSystemCommand.m
@@ -0,0 +1,10 @@
+%{
+Purpose: This function runs system commands and checks to make sure that
+there are no attempts to do anything that shouldn't be done
+%}
+
+function [successFlag, output] = runSystemCommand(command,quietFlag)
+
+    
+
+end
