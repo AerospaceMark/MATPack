60a841ebcd6f7abd5b74d834f4ef0dbd6a5bb426
Mark C. Anderson <anderson.mark.az@gmail.com>
2021-03-03 14:28:59 -0700

    Improved safety

--- src/addPackage.m
+++ src/addPackage.m
@@ -1,23 +1,8 @@
 % addPackage(pathToPackage,packageName)
 % 
-% This is a function that adds a MATLAB package to your path. There are
-% three cases:
-%
-% 1. Add a package that already exists in the userpath folder. This will do
-% nothing because the package already exists there.
-%       - pathToPackage = just the name of the package (eg.
-%       'General-Signal-Processing')
-%       - packageName = empty
-%
-% 2. Add a package that exists in a specified path on your computer. This
-% will then copy the entire package into the userpath folder. Updates made
-% in the original folder will not be reflected in the userpath folder
-% unless they are pushed from the original folder to an online Git provider
-% and then pulled into the userpath folder.
-%       - pathToPackage = the absolute path to the package on your computer
-%       - packageName = empty
-%
-% 3. Clone a package from an online Git repository
+% This is a function that adds a MATLAB package to your path.
+
+% - Clone a package from an online Git repository
 %       - pathToPackage = the cloning url
 %       - packageName = the name you want to give the package. If left
 %       empty, it defaults to the default cloning name.
@@ -27,17 +12,8 @@
 
 function addPackage(pathToPackage,packageName)
     
-    % 1. Seeing if the package exists in the user directory (Documents/MATLAB)
-    if isfolder(strcat(userpath,filesep,pathToPackage))
-        disp(strcat("Package already exists at ",userpath,filesep,pathToPackage))
-    
-    % 2. Seeing if the package exists elsewhere on the computer
-    elseif isfolder(pathToPackage)
-        eval(strcat("!cp -r ",convertCharsToStrings(pathToPackage)," ",userpath))
-        disp(strcat("Copied ",convertCharsToStrings(pathToPackage)," to ",userpath,"."))
-    
-    % 3. Clone the package from an online Git repository 
-    elseif pathToPackage(end-3:end) == '.git'
+    % Clone the package from an online Git repository 
+    if pathToPackage(end-3:end) == '.git'
         disp(strcat("Cloning ",pathToPackage," into ",userpath))
         
         if nargin < 2
@@ -48,11 +24,33 @@ function addPackage(pathToPackage,packageName)
                                                                  % the '.git'
         end
         
-        eval(strcat("!git clone ",convertCharsToStrings(pathToPackage)," ",userpath,filesep,convertCharsToStrings(packageName)))
+        if isfolder([userpath,filesep,packageName])
+            alreadyExists(packageName)
+            return
+        end
+        
+        command = strcat("git clone ",convertCharsToStrings(pathToPackage),...
+            " ",userpath,filesep,convertCharsToStrings(packageName));
+        
+        [successFlag, output] = runSystemCommand(command,false);
+        
+        if ~successFlag
+            
+            disp('Error while adding the package. System output is:')
+            disp(output)
+            
+        end
         
     else
+        
         disp('Unable to add package.')
         
     end
+
+end
+
+function alreadyExists(packageName)
+
+    disp(['Package "',packageName,'" already exists in ',userpath])
 
 end
--- src/runSystemCommand.m
+++ src/runSystemCommand.m
@@ -3,8 +3,66 @@ Purpose: This function runs system commands and checks to make sure that
 there are no attempts to do anything that shouldn't be done
 %}
 
-function [successFlag, output] = runSystemCommand(command,quietFlag)
+function [successFlag, output] = runSystemCommand(command,showOutput)
 
-    
+    if nargin < 2
+        showOutput = false;
+    end
+
+    % If no bad/dangerous symbols are contained, then go ahead and run the
+    % command
+    if ~containsBadSymbol(command)
+        
+        % Running the command
+        [successFlag, output] = system(command);
+        
+        % Making a "true" value be the case where the command was
+        % successful
+        successFlag = ~successFlag;
+        
+        % Showing the output
+        if showOutput
+            
+            disp(output)
+            
+        end
+        
+    else % if a bad symbol was found in the command string
+        
+        successFlag = false;
+        output = 'Bad symbol used in command, command not run.';
+
+    end
+
+end
+
+function badSymbol = containsBadSymbol(command)
+
+    if contains(command,'!')
+        badSymbol = true;
+    elseif contains(command,'&')
+        badSymbol = true;
+    elseif contains(command," . ") % period with spaces around it
+        badSymbol = true;
+    elseif contains(command,'..')
+        badSymbol = true;
+    elseif contains(command,';')
+        badSymbol = true;
+    elseif contains(command,'#')
+        badSymbol = true;
+    elseif contains(command,'?')
+        badSymbol = true;
+    elseif contains(command,'`')
+        badSymbol = true;
+    elseif contains(command,'~')
+        badSymbol = true;
+    elseif contains(command,'$')
+        badSymbol = true;
+    elseif contains(command,'|')
+        badSymbol = true;
+    else
+        badSymbol = false;
+    end
+
 
 end
