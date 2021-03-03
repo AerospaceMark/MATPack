639610bb3bebb4713dbf00851787e833c7d15ecf
Mark C. Anderson <anderson.mark.az@gmail.com>
2021-03-03 14:46:20 -0700

    Works with safe system commands

--- src/packageStatus.m
+++ src/packageStatus.m
@@ -4,6 +4,15 @@ function packageStatus(packageName)
 
     packageName = convertCharsToStrings(packageName);
     
-    eval(strcat("!git -C ",userpath,filesep,packageName," status"));
+    command = strcat("git -C ",userpath,filesep,packageName," status");
+    
+    [successFlag, output] = runSystemCommand(command,true);
+        
+    if ~successFlag
+
+        disp('Error while checking the package status. System output is:')
+        disp(output)
+
+    end
 
 end
