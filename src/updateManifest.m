c7f312248b11bdce77dfa422096c221ed0cc135f
Mark C. Anderson <anderson.mark.az@gmail.com>
2021-03-17 17:02:10 -0600

    Added feature to auto-commit

--- src/updateManifest.m
+++ src/updateManifest.m
@@ -1,7 +1,11 @@
 % Purpose: To create an easy way to update all of the package commit IDs to
 % the top of the master branch
 
-function updateManifest(package)
+function updateManifest(package,commit)
+
+    if nargin < 2
+        commit = false;
+    end
 
     if nargin < 1 % If no package is specified, use the current package
         % Getting the name of the current package
@@ -40,6 +44,34 @@ function updateManifest(package)
         end
         
         disp(strcat("Updated 'Manifest.csv' in the '",package,"' package."))
+        
+        % Make this update a commit
+        if commit == true
+            
+            command = strcat("git -C ",userpath,filesep,package," add Manifest.csv");
+            
+            [successFlag, output] = runSystemCommand(command,true);
+        
+            if ~successFlag
+
+                disp('Error while staging the Manifest file. System output is:')
+                disp(output)
+                return
+
+            end
+            
+            command = strcat("git -C ",userpath,filesep,package," commit -m ",'"',"Automatic_Update_Manifest.csv",'"');
+            
+            [successFlag, output] = runSystemCommand(command,true);
+        
+            if ~successFlag
+
+                disp('Error while committing the Manifest file. System output is:')
+                disp(output)
+
+            end
+            
+        end
         
     else
         disp(strcat("No Manifest file detected for the '",package,"' package."))
